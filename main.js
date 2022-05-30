9c488c13ff384a3b9539a5737935a92554e3c9a2
Mkoys <mkoys@protonmail.com>
2022-05-30 18:04:22 +0200

    +Basic typing

--- main.js
+++ main.js
@@ -12,5 +12,28 @@ const carrot  = new Carrot();
 
 // Handles keyboard input
 function handleKeyboard(event) {
+    const spaceKey = event.keyCode == 32; // Space key
 
+    const aplhaNumKeys = event.keyCode > 47 && event.keyCode < 91; // AplhaNumeric key
+    const numericKeys = event.keyCode > 95 && event.keyCode < 112; // Numberic key
+    const specialKeys = event.keyCode > 183 && event.keyCode < 224; // Spcial character key
+    const basicKeys = aplhaNumKeys || numericKeys || specialKeys; // Basic typing keys together
+
+    // On basic key press
+    if(basicKeys) {
+        // Add event character on set line and column into editor
+        editor.addContent(carrot.line, carrot.column, event.key);
+        // Increment carrot column
+        carrot.column++;
+    }
+
+    // On space key press
+    if(spaceKey) {
+        // Add empty space on set line and column into editor
+        editor.addContent(carrot.line, carrot.column, "\xa0");
+        // Increment carrot column
+        carrot.column++;
+    }
+
+    console.log(editor.content);
 }
