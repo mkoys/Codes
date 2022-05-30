c8dfcebcc9e7a1606a088e49119b68fadc244856
Mkoys <mkoys@protonmail.com>
2022-05-30 17:54:16 +0200

    +Carrot

---
+++ Carrot.js
@@ -0,0 +1,9 @@
+// Carrot is a typing currsor class
+export default class Carrot {
+    constructor(line, column) {
+        // X Position of carrot
+        this.line = line ? line : 0; // Set line to provided else 0
+        // Y Position of carrot
+        this.column = column ? column : 0; // Set column to provided else 0
+    }
+}
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // Import classes
 import Editor from "./Editor.js";
+import Carrot from "./Carrot.js";
 
 // Create keydown event listener and pass it to handler
 document.addEventListener("keydown", event => handleKeyboard(event));
 
 // Create new editor
 const editor = new Editor();
+// Create new carrot with default postion
+const carrot  = new Carrot();
 
 // Handles keyboard input
 function handleKeyboard(event) {
