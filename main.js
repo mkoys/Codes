e861a1fc1b67051bba658e5bc72809c1d144bbc1
Mkoys <mkoys@protonmail.com>
2022-06-01 17:18:35 +0200

    +Moduling

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ import Carrot from "./Carrot.js";
 import View from "./View.js";
 import Marker from "./Marker.js";
 
-// Create keydown event listener and pass it to handler
-document.addEventListener("keydown", event => handleKeyboard(event));
-
 // Create new editor
 const editor = new Editor();
 // Create new carrot with default postion
@@ -15,6 +12,8 @@ const carrot = new Carrot();
 const view = new View(document.querySelector("main"));
 // Create new highlighter
 const marker = new Marker();
+// Scheme storage
+let scheme = null;
 
 // Set font width and height
 view.fontHeight = 26;
@@ -22,128 +21,135 @@ view.fontWidth = 13.2;
 // Add carrot to view
 const carrotID = view.addCarrot();
 
-// Handles keyboard input
-function handleKeyboard(event) {
-    const spaceKey = event.keyCode == 32; // Space key
-    const removeKey = event.keyCode == 8; // Remove key
-    const newLineKey = event.keyCode == 13; // New Line key
+// Method for setting scheme
+function setScheme(newScheme) {
+    scheme = newScheme;
+}
 
-    const upKey = event.keyCode == 38; // Up key
-    const downKey = event.keyCode == 40; // Down key
-    const leftKey = event.keyCode == 37; // left key
-    const rightKey = event.keyCode == 39; // right key
+// Add content method
+function addContent(char) {
+    // Add event character on set line and column into editor
+    editor.addContent(carrot.line, carrot.column, char);
+    // Increment carrot column
+    carrot.column++;
+    // Get tokenized content
+    const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
+    // Update line with tokenized content
+    view.updateLine(carrot.line, markedContent);
+    // Updates main carrot's position
+    view.updateCarrot(carrotID, carrot.column, carrot.line);
+}
 
-    const aplhaNumKeys = event.keyCode > 47 && event.keyCode < 91; // AplhaNumeric key
-    const numericKeys = event.keyCode > 95 && event.keyCode < 112; // Numberic key
-    const specialKeys = event.keyCode > 183 && event.keyCode < 224; // Spcial character key
-    const basicKeys = aplhaNumKeys || numericKeys || specialKeys; // Basic typing keys together
-
-    // Scheme for syntax Highlighting
-    const scheme = {
-        default: /(?<let>\blet\b)|(?<const>\bconst\b)|(?<operator>\-\=|\+\=|\-|\+|\\|\/|\%)|(?<signs>\=|\,)|(?<number>\b[0-9]+\b)|(?<func>\bfunction\b\s+\w+(\s+|)\(.*\))|(?<function>\bfunction\b)/gm,
-        func: /(?<function>\bfunction\b)|(?<params>\(.*\))|(?<functionName>\w)/gm,
-        params: /(?<bracket>\(|\))|(?<default>.*)/gm
-    }
-
-    // On basic key press
-    if (basicKeys) {
-        // Add event character on set line and column into editor
-        editor.addContent(carrot.line, carrot.column, event.key);
-        // Increment carrot column
-        carrot.column++;
+// Method remove content
+function removeContent() {
+    if (!carrot.column) {
+        // If line is 0 return
+        if (carrot.line == 0) { return }
+        // Set column to previous line length
+        carrot.column = editor.lineLength(carrot.line - 1);
+        // Remove line from editor
+        editor.removeLine(carrot.line);
+        // Remove line from view
+        view.removeLine(carrot.line);
+        // Decrement carrot line
+        carrot.line--;
+        // Get tokenized content
+        const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
+        // Update line with tokenized content
+        view.updateLine(carrot.line, markedContent);
+    } else {
+        // Remove one character on current line and column
+        editor.removeContent(carrot.line, carrot.column, 1);
+        // Decrement column
+        carrot.column--;
         // Get tokenized content
         const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
         // Update line with tokenized content
         view.updateLine(carrot.line, markedContent);
     }
+    // Updates main carrot's position
+    view.updateCarrot(carrotID, carrot.column, carrot.line);
+}
 
-    // On space key press
-    if (spaceKey) {
-        // Add empty space on set line and column into editor
-        editor.addContent(carrot.line, carrot.column, "\xa0");
-        // Increment carrot column
-        carrot.column++;
-        // Get tokenized content
-        const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
-        // Update line with tokenized content
-        view.updateLine(carrot.line, markedContent);
-    }
+// Method new line
+function newLine() {
+    // Append line on carrot line and column
+    editor.appendLine(carrot.line, carrot.column);
+    // Get tokenized content
+    const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
+    // Update line with tokenized content
+    view.updateLine(carrot.line, markedContent);
+    // Inciment carrot line
+    carrot.line++;
+    // Reset carrot column
+    carrot.column = 0;
+    // Get tokenized content
+    const markedAppend = marker.mark(editor.content[carrot.line], scheme.default, scheme);
+    // Append the new line into view
+    view.appendLine(carrot.line, markedAppend);
+    // Updates main carrot's position
+    view.updateCarrot(carrotID, carrot.column, carrot.line);
+}
 
-    // On remove key press
-    if (removeKey) {
-        if (!carrot.column) {
-            // If line is 0 return
-            if (carrot.line == 0) { return }
-            // Set column to previous line length
-            carrot.column = editor.lineLength(carrot.line - 1);
-            // Remove line from editor
-            editor.removeLine(carrot.line);
-            // Remove line from view
-            view.removeLine(carrot.line);
-            // Decrement carrot line
-            carrot.line--;
-            // Get tokenized content
-            const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
-            // Update line with tokenized content
-            view.updateLine(carrot.line, markedContent);
-        } else {
-            // Remove one character on current line and column
-            editor.removeContent(carrot.line, carrot.column, 1);
-            // Decrement column
-            carrot.column--;
-            // Get tokenized content
-            const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
-            // Update line with tokenized content
-            view.updateLine(carrot.line, markedContent);
-        }
-    }
-
-    // On new line key press
-    if (newLineKey) {
-        // Append line on carrot line and column
-        editor.appendLine(carrot.line, carrot.column);
-        // Get tokenized content
-        const markedContent = marker.mark(editor.content[carrot.line], scheme.default, scheme);
-        // Update line with tokenized content
-        view.updateLine(carrot.line, markedContent);
-        // Inciment carrot line
+// Method move down
+function moveDown() {
+    // Check if we are at the end of file
+    if (carrot.line < editor.content.length) {
+        // Increment carrot line
         carrot.line++;
-        // Reset carrot column
-        carrot.column = 0;
-        // Get tokenized content
-        const markedAppend = marker.mark(editor.content[carrot.line], scheme.default, scheme);
-        // Append the new line into view
-        view.appendLine(carrot.line, markedAppend);
-    }
-
-    if(downKey) {
-        if(carrot.line < editor.content.length) {
-            carrot.line++;
-        }
-    }
-
-    if(upKey) {
-        if(carrot.line > 0) {
-            carrot.line--;
-        }
-    }
-
-    if(leftKey) {
-        if(carrot.column > 0) {
-            carrot.column--;
-        }else {
-
-        }
-    }
-
-    if(rightKey) {
-        if(carrot.column < editor.lineLength(carrot.line)) {
-            carrot.column++;
-        }else {
-            
+        // Check if we aren't exceeding line length
+        if (editor.lineLength(carrot.line) < carrot.column) {
+            // Set carrot column to current line length
+            carrot.column = editor.lineLength(carrot.line);
         }
     }
 
     // Updates main carrot's position
     view.updateCarrot(carrotID, carrot.column, carrot.line);
 }
+
+// Method move up
+function moveUp() {
+    // Check if we are at start of file
+    if (carrot.line > 0) {
+        // Decrement carrot line
+        carrot.line--;
+        // Check if we aren't exceeding line length
+        if (editor.lineLength(carrot.line) < carrot.column) {
+            // Set carrot column to current line length
+            carrot.column = editor.lineLength(carrot.line);
+        }
+    }
+
+    // Updates main carrot's position
+    view.updateCarrot(carrotID, carrot.column, carrot.line);
+}
+
+// Method move left
+function moveLeft() {
+    // Check if we are at start of line
+    if (carrot.column > 0) {
+        // Decrement carrot column
+        carrot.column--;
+    } else {
+
+    }
+
+    // Updates main carrot's position
+    view.updateCarrot(carrotID, carrot.column, carrot.line);
+}
+
+// Method move right
+function moveRight() {
+    // Check if we are at end of line
+    if (carrot.column < editor.lineLength(carrot.line)) {
+        // Increment carrot column
+        carrot.column++;
+    } else {
+
+    }
+    // Updates main carrot's position
+    view.updateCarrot(carrotID, carrot.column, carrot.line);
+}
+
+export { setScheme,moveDown, moveUp, moveLeft, moveRight, addContent, removeContent, newLine }
---
+++ script.js
@@ -0,0 +1,62 @@
+import {setScheme,addContent, removeContent, newLine, moveDown, moveRight, moveLeft, moveUp} from "./main.js";
+
+// Create keydown event listener and pass it to handler
+document.addEventListener("keydown", event => handleKeyboard(event));
+
+// Scheme for syntax Highlighting
+const scheme = {
+    default: /(?<let>\blet\b)|(?<const>\bconst\b)|(?<operator>\-\=|\+\=|\-|\+|\\|\/|\%)|(?<signs>\=|\,)|(?<number>\b[0-9]+\b)|(?<func>\bfunction\b\s+\w+(\s+|)\(.*\))|(?<function>\bfunction\b)/gm,
+    func: /(?<function>\bfunction\b)|(?<params>\(.*\))|(?<functionName>\w)/gm,
+    params: /(?<bracket>\(|\))|(?<default>.*)/gm
+}
+
+setScheme(scheme);
+
+// Handles keyboard input
+function handleKeyboard(event) {
+    const spaceKey = event.keyCode == 32; // Space key
+    const removeKey = event.keyCode == 8; // Remove key
+    const newLineKey = event.keyCode == 13; // New Line key
+
+    const upKey = event.keyCode == 38; // Up key
+    const downKey = event.keyCode == 40; // Down key
+    const leftKey = event.keyCode == 37; // left key
+    const rightKey = event.keyCode == 39; // right key
+
+    const aplhaNumKeys = event.keyCode > 47 && event.keyCode < 91; // AplhaNumeric key
+    const numericKeys = event.keyCode > 95 && event.keyCode < 112; // Numberic key
+    const specialKeys = event.keyCode > 183 && event.keyCode < 224; // Spcial character key
+    const basicKeys = aplhaNumKeys || numericKeys || specialKeys; // Basic typing keys together
+
+    if(spaceKey) {
+        addContent("\xa0");
+    }
+
+    if(basicKeys) {
+        addContent(event.key);
+    }
+
+    if(removeKey) {
+        removeContent();
+    }
+
+    if(newLineKey) {
+        newLine();
+    }
+
+    if(rightKey) {
+        moveRight();
+    }
+
+    if(leftKey) {
+        moveLeft();
+    }
+
+    if(downKey) {
+        moveDown();
+    }
+
+    if(upKey) {
+        moveUp();
+    }
+}
