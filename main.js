6acde12b97b7d2b9a5b7cf84ed326e0291b5a97f
Mkoys <mkoys@protonmail.com>
2022-06-01 15:09:02 +0200

    +Syntax highlight

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function handleKeyboard(event) {
 
     // Scheme for syntax Highlighting
     const scheme = {
-        default: /(?<let>\blet\b)|(?<const>\bconst\b)|(?<operator>\-\=|\+\=|\-|\+|\\|\/|\%)|(?<signs>\=|\,)|(?<number>\b[0-9]+\b)|(?<func>\bfunction\b\b.*\b\(.*\))|(?<function>\bfunction\b)/gm,
+        default: /(?<let>\blet\b)|(?<const>\bconst\b)|(?<operator>\-\=|\+\=|\-|\+|\\|\/|\%)|(?<signs>\=|\,)|(?<number>\b[0-9]+\b)|(?<func>\bfunction\b\s+\w+(\s+|)\(.*\))|(?<function>\bfunction\b)/gm,
         func: /(?<function>\bfunction\b)|(?<params>\(.*\))|(?<functionName>\w)/gm,
         params: /(?<bracket>\(|\))|(?<default>.*)/gm
     }
