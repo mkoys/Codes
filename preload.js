50a9719c8cb65f8068a8047870a48c2aef5f0f8b
Mkoys <mkoys@protonmail.com>
2022-06-05 10:03:16 +0200

    +Nodemon

--- preload.js
+++ preload.js
@@ -1,2 +1,7 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+// On document load
+window.addEventListener('DOMContentLoaded', () => {
+
+});
